a16c6d6b1c6aa93e59d9464e1b5d14f6d15ae6ad
Alex <alexmlrdv@gmail.com>
2021-08-03 22:43:05 +0200

    hooks training

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,10 @@
 		<HelloWorld
 			msg="Welcome to Your Vue.js App"
 		/>
-		<v-calculator class="v-calculator" msg="Vue.JS 2.0" />
+		<v-calculator class="v-calculator" msg="Vue.JS 2.0"></v-calculator>
+		<counter-button v-if="show"></counter-button>
+		<counter-button v-show="show"></counter-button>
+		<button @click="show=!show">Скрыть / Отобразить</button>
 
 	</div>
 </template>
@@ -13,12 +16,14 @@
 import HelloWorld from './components/HelloWorld.vue';
 import Calculator from './components/v-calculator.vue';
 import logoVue from './assets/logo.png';
+import CounterButton from './components/CounterButton.vue';
 
 export default {
 	name: 'App',
 	data() {
 		return {
 			srcLogo: logoVue,
+			show: true,
 		}
 	},
 	components: {
@@ -27,6 +32,8 @@ export default {
 		//'Calculator': Calculator
 		//Calculator: Calculator
 		//Calculator
-	}
+		'counter-button': CounterButton,
+	},
+
 }
 </script>
