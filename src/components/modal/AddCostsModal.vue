e99caff456410b5f9b8d9437ef3ca4b0236f2314
Alex <alexmlrdv@gmail.com>
2021-08-13 19:30:28 +0200

    HW-6 continue

---
+++ src/components/modal/AddCostsModal.vue
@@ -0,0 +1,25 @@
+<template>
+   <div class="wrapper">
+      <div class="header">
+
+      </div>
+      <div class="content">
+         form
+      </div>
+      <div class="footer">
+         <button
+            class="btn btn-info"
+            @click="onClose"
+         >Close
+         </button>
+      </div>
+   </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+
+</style>
