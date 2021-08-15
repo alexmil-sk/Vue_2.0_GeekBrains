ef6505d115b8a46638eff1c4ade754ab53b6eb7f
alexmil-sk <75791169+alexmil-sk@users.noreply.github.com>
2021-08-15 08:47:50 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -21,17 +21,6 @@ export default {
       'the-header': Header,
       'app-main': AppMain,
      },
-  provide() {
-    return {
-      emails: [
-        {id: 1, theme: 'Купил себе PlayStation 5'},
-        {id: 2, theme: 'Выучил Vue Router'},
-        {id: 3, theme: 'Хочу изучить весь Vue'},
-        {id: 4, theme: 'А следующий блок про Vuex!'},
-        {id: 5, theme: 'А что там на счет Vue Hooks?'}
-      ]
-    }
-  }
 }
 </script>
 
