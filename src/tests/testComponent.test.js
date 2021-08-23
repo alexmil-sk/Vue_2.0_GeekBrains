77234d999830f9bf8209000448d64f8d682eae5c
Alex <alexmlrdv@gmail.com>
2021-08-23 07:17:05 +0200

    ...

---
+++ src/tests/testComponent.test.js
@@ -0,0 +1,9 @@
+import TestComponent from './TestComponent';
+import { mount } from '@vue/test-utils';
+import { describe } from 'yargs';
+
+describe('TestComponent', () => {
+	test('Content of TestComponent', () => {
+		
+	});
+});
