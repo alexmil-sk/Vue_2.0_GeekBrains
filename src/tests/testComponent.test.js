06743874efc51eb1da357d4868779d5b0754c570
Alex <alexmlrdv@gmail.com>
2021-08-26 14:37:27 +0200

    HW-8 for installing vuetify

--- src/tests/testComponent.test.js
+++
@@ -1,9 +0,0 @@
-import TestComponent from './TestComponent';
-import { mount } from '@vue/test-utils';
-import { describe } from 'yargs';
-
-describe('TestComponent', () => {
-	test('Content of TestComponent', () => {
-		
-	});
-});
